fbd842d1ff5a177ccc621ee1c844a1eaf5f88e28
3top1a <3top1a.official@gmail.com>
2022-03-23 16:37:27 +0100

    on redraw

--- src/ui.rs
+++ src/ui.rs
@@ -6,10 +6,10 @@ use glium::*;
 
 struct WindowData {
 	// OpenGl
-
+	
 	gl_event_loop: glutin::event_loop::EventLoop<()>,
 	gl_display: glium::Display,
-
+	
 	// ImGui
 	im_builder: imgui::Context,
 	im_renderer: imgui_glium_renderer::Renderer,
@@ -25,23 +25,23 @@ create_window()
 		.with_title("Asd")
 		.with_decorations(false)
 		.with_inner_size(glutin::dpi::LogicalSize::new(1024f64, 768f64));
-
+	
 	let context_builder = glutin::ContextBuilder::new()
 		.with_vsync(false)
 		.with_hardware_acceleration(Some(true))
 		.with_multisampling(2);
-
+	
 	let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
-
+	
 	// Create ImGui
 	let mut imgui_builder = imgui::Context::create();
 	imgui_builder.set_ini_filename(None);
 	imgui_builder.style_mut()
 	.use_dark_colors();
 	imgui_builder.style_mut();
-
+	
 	let imgui_renderer = imgui_glium_renderer::Renderer::init(&mut imgui_builder, &display).unwrap();
-
+	
 	WindowData {
 		gl_event_loop: event_loop,
 		gl_display: display,
@@ -53,54 +53,90 @@ create_window()
 fn
 window_loop( mut data: WindowData )
 {
-	//let mut data = wdata;
-
 	data.gl_event_loop.run(move |event, _, control_flow| {
 		let event_ref = &event;
-
+		
 		// Close
 		if let glutin::event::Event::WindowEvent { event, .. } = event_ref {
-            match event {
-                glutin::event::WindowEvent::CloseRequested |
-                glutin::event::WindowEvent::KeyboardInput {
-                    input: glutin::event::KeyboardInput {
-                        virtual_keycode: Some(glutin::event::VirtualKeyCode::Escape),
-                        ..
-                    },
-                    ..
-                } => {
+			match event {
+				glutin::event::WindowEvent::CloseRequested |
+				glutin::event::WindowEvent::KeyboardInput {
+					input: glutin::event::KeyboardInput {
+						virtual_keycode: Some(glutin::event::VirtualKeyCode::Escape),
+						..
+					},
+					..
+				} => {
 					*control_flow = glutin::event_loop::ControlFlow::Exit;
 					return
 				},
-                _ => (),
-            }
-        }
+				_ => (),
+			}
+		}
+		std::mem::drop(event_ref);
 
-		// *Render
+		//* Draw
+		if let glutin::event::Event::RedrawRequested { .. } = event_ref {
+			// Create render target
+			let mut target = data.gl_display.draw();
+			
+			// Background
+			target.clear_color(0.05, 0.05, 0.05, 1.0);
+			
+			// ImGui IO
+			let mut imgui_io = data.im_builder.io_mut();
+			
+			// Set display dimentions
+			let (width, height) = data.gl_display.get_framebuffer_dimensions();
+			imgui_io.display_size = [width as f32, height as f32];
+			
+			// Make a frame
+			let ui = data.im_builder.frame();
 
-		// Create render target
-		let mut target = data.gl_display.draw();
-
-		// Background
-		target.clear_color(0.05, 0.05, 0.05, 1.0);
-
-		// ImGui IO
-		let mut imgui_io = data.im_builder.io_mut();
-
-		// Set display dimentions
-		let (width, height) = data.gl_display.get_framebuffer_dimensions();
-		imgui_io.display_size = [width as f32, height as f32];
+			
+			// Add a test window
+			imgui::Window::new(imgui::im_str!("Test window"))
+			.size([300.0, 100.0], imgui::Condition::FirstUseEver)
+			.build(&ui, || {
+				ui.text("Hello world!");
+				ui.text("This...is...imgui-rs!");
+				ui.button(imgui::im_str!("Test"), [60.0, 20.0]);
+				ui.separator();
+			});
+			imgui::Window::new(imgui::im_str!("Debug"))
+			.size([350.0, 60.0], imgui::Condition::FirstUseEver)
+			.position([(width / 2) as f32, 10.0], imgui::Condition::Always)
+			.bg_alpha(0.25)
+			.scrollable(false)
+			.collapsible(false)
+			.movable(false)
+			.no_decoration()
+			.scroll_bar(false)
+			.resizable(false)
+			.build(&ui, || {
+				ui.text("--- Debug menu ---");
+				ui.separator();
+				ui.text( format!("Free VRAM: {}KB", data.gl_display.get_free_video_memory().unwrap_or(usize::MIN)));
+				//ui.text( format!("Reported FPS: {}", &imgui_io.framerate  ))
+			});
+			
+			// Render that ImGui frame to target
+			data.im_renderer.render(&mut target, ui.render()).unwrap();
+			
+			target.finish().unwrap();
+		}
 		
 		// Set mouse stuff
+		let mut imgui_io = data.im_builder.io_mut();
 		if let glutin::event::Event::WindowEvent { event , .. } = event_ref
 		{
 			match event {
-				glutin::event::WindowEvent::CursorMoved { device_id, position, modifiers } => {
+				glutin::event::WindowEvent::CursorMoved { position , ..} => {
 					// TODO Better mouse movement
 					// This has a lot of delay when dragging
 					imgui_io.mouse_pos = [position.x as f32, position.y as f32];
 				}
-				glutin::event::WindowEvent::MouseInput { device_id, state, button, modifiers } =>
+				glutin::event::WindowEvent::MouseInput { state, button, ..} =>
 				{
 					// TODO Better mouse input
 					// This is really jank and is only for M1
@@ -115,36 +151,19 @@ window_loop( mut data: WindowData )
 						imgui_io.mouse_down = [ s , false, false, false, false ]
 					}
 				}
-			_ => ()
+				_ => ()
 			}
 		}
-
-		// Make a frame
-		let ui = data.im_builder.frame();
-
-		// Add a test window
-		imgui::Window::new(imgui::im_str!("Hello world"))
-			.size([300.0, 100.0], imgui::Condition::FirstUseEver)
-			.build(&ui, || {
-				ui.text("Hello world!");
-				ui.text("This...is...imgui-rs!");
-				ui.separator();
-			});
-
-		// Render that ImGui frame to target
-		data.im_renderer.render(&mut target, ui.render()).unwrap();
-
-		target.finish().unwrap();
+		std::mem::drop(imgui_io);
 	});
 }
-
 
 pub fn
 window()
 {
 	//* Init
 	let data = create_window();
-
+	
 	//* Loop
 	window_loop(data);
 }
