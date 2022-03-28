3b02333821d9a4bf70c917b7f3c5581c20deb5e0
3top1a <3top1a.official@gmail.com>
2022-03-28 08:40:41 +0200

    Draw extracted

--- src/ui.rs
+++ src/ui.rs
@@ -65,6 +65,166 @@ impl WindowData {
 	)
 	}
 
+	fn draw(&mut self)
+	{
+		// Create render target
+		let mut target = self.gl_display.draw();
+
+		// Background
+		target.clear_color(0.05, 0.05, 0.05, 1.0);
+
+		// ImGui IO
+		let framerate = self.im_builder.io().framerate;
+		let delta = self.im_builder.io().delta_time;
+		let mut imgui_io = self.im_builder.io_mut();
+
+		// Set display dimentions
+		let (width, height) = self.gl_display.get_framebuffer_dimensions();
+		imgui_io.display_size = [width as f32, height as f32];
+
+		// Make a frame
+		let ui = self.im_builder.frame();
+
+		// Add a test window
+		imgui::Window::new(imgui::im_str!("Test window"))
+			.size([300.0, 100.0], imgui::Condition::FirstUseEver)
+			.build(&ui, || {
+				ui.text("Hello world!");
+				ui.text("This...is...imgui-rs!");
+				ui.button(imgui::im_str!("Test"), [60.0, 20.0]);
+				ui.separator();
+			});
+
+		// Add the debug window
+		imgui::Window::new(imgui::im_str!("Debug"))
+			.size([350.0, 100.0], imgui::Condition::FirstUseEver)
+			.position(
+				[(width as f32 / 2f32) - (350.0 / 2.0), 10.0],
+				imgui::Condition::Always,
+			)
+			.bg_alpha(0.25)
+			.scrollable(false)
+			.collapsible(false)
+			.movable(false)
+			.no_decoration()
+			.scroll_bar(false)
+			.resizable(false)
+			.build(&ui, || {
+				ui.text("Debug menu");
+				ui.separator();
+				ui.text(format!(
+					"Free VRAM: {}MB",
+					self.gl_display
+						.get_free_video_memory()
+						.unwrap_or(usize::MIN) / 1_000_000
+				));
+				ui.text(format!("Reported FPS: {}", framerate));
+				ui.text(format!("Delta: {}", delta));
+				ui.text(format!("Calculated FPS: {}", 1.0 / delta));
+			});
+
+		// Triangle
+		let vertex_buffer = {
+			glium::VertexBuffer::new(
+				&self.gl_display,
+				&[
+					Vertex {
+						position: [0.0, 0.0],
+						tex_coords: [0.0, 0.0],
+					},
+					Vertex {
+						position: [0.0, 1.0],
+						tex_coords: [0.0, 1.0],
+					},
+					Vertex {
+						position: [1.0, 1.0],
+						tex_coords: [1.0, 1.0],
+					},
+					Vertex {
+						position: [1.0, 0.0],
+						tex_coords: [1.0, 0.0],
+					},
+				],
+			)
+			.unwrap()
+		};
+		let index_buffer = glium::IndexBuffer::new(
+			&self.gl_display,
+			glium::index::PrimitiveType::TriangleStrip,
+			&[1 as u16, 2, 0, 3],
+		)
+		.unwrap();
+
+		// Image
+		let image = image::load(
+			Cursor::new(&include_bytes!("../example.png")),
+			image::ImageFormat::Png,
+		)
+		.unwrap()
+		.to_rgba8();
+		let image_dimensions = image.dimensions();
+		let image = glium::texture::RawImage2d::from_raw_rgba_reversed(
+			&image.into_raw(),
+			image_dimensions,
+		);
+		let texture = glium::texture::SrgbTexture2d::new(&self.gl_display, image).unwrap();
+
+		let uniforms = uniform! {
+			matrix: [
+				[1.0, 0.0, 0.0, 0.0],
+				[0.0, 1.0, 0.0, 0.0],
+				[0.0, 0.0, 1.0, 0.0],
+				[0.0, 0.0, 0.0, 1.0f32],
+			],
+			tex: &texture,
+		};
+
+		let vertex_shader_src = r#"
+			#version 140
+			in vec2 position;
+			in vec2 tex_coords;
+			out vec2 v_tex_coords;
+			uniform mat4 matrix;
+			void main() {
+				v_tex_coords = tex_coords;
+				gl_Position = matrix * vec4(position, 0.0, 1.0);
+			}
+		"#;
+
+		let fragment_shader_src = r#"
+			#version 140
+			in vec2 v_tex_coords;
+			out vec4 color;
+			uniform sampler2D tex;
+			void main() {
+				color = texture(tex, v_tex_coords);
+			}
+		"#;
+
+		let program = glium::Program::from_source(
+			&self.gl_display,
+			vertex_shader_src,
+			fragment_shader_src,
+			None,
+		)
+		.unwrap();
+
+		target
+			.draw(
+				&vertex_buffer,
+				&index_buffer,
+				&program,
+				&uniforms,
+				&Default::default(),
+			)
+			.unwrap();
+
+		// Render that ImGui frame to target
+		self.im_renderer.render(&mut target, ui.render()).unwrap();
+
+		target.finish().unwrap();
+	}
+
 	fn window_loop(mut self, event: glutin::event_loop::EventLoop<()> ) {
 		event.run(move |event, _, control_flow| {
 			let event_ref = &event;
@@ -91,162 +251,7 @@ impl WindowData {
 
 			//* Draw
 			if let glutin::event::Event::RedrawRequested { .. } = event_ref {
-				// Create render target
-				let mut target = self.gl_display.draw();
-
-				// Background
-				target.clear_color(0.05, 0.05, 0.05, 1.0);
-
-				// ImGui IO
-				let framerate = self.im_builder.io().framerate;
-				let delta = self.im_builder.io().delta_time;
-				let mut imgui_io = self.im_builder.io_mut();
-
-				// Set display dimentions
-				let (width, height) = self.gl_display.get_framebuffer_dimensions();
-				imgui_io.display_size = [width as f32, height as f32];
-
-				// Make a frame
-				let ui = self.im_builder.frame();
-
-				// Add a test window
-				imgui::Window::new(imgui::im_str!("Test window"))
-					.size([300.0, 100.0], imgui::Condition::FirstUseEver)
-					.build(&ui, || {
-						ui.text("Hello world!");
-						ui.text("This...is...imgui-rs!");
-						ui.button(imgui::im_str!("Test"), [60.0, 20.0]);
-						ui.separator();
-					});
-
-				// Add the debug window
-				imgui::Window::new(imgui::im_str!("Debug"))
-					.size([350.0, 100.0], imgui::Condition::FirstUseEver)
-					.position(
-						[(width as f32 / 2f32) - (350.0 / 2.0), 10.0],
-						imgui::Condition::Always,
-					)
-					.bg_alpha(0.25)
-					.scrollable(false)
-					.collapsible(false)
-					.movable(false)
-					.no_decoration()
-					.scroll_bar(false)
-					.resizable(false)
-					.build(&ui, || {
-						ui.text("Debug menu");
-						ui.separator();
-						ui.text(format!(
-							"Free VRAM: {}MB",
-							self.gl_display
-								.get_free_video_memory()
-								.unwrap_or(usize::MIN) / 1_000_000
-						));
-						ui.text(format!("Reported FPS: {}", framerate));
-						ui.text(format!("Delta: {}", delta));
-						ui.text(format!("Calculated FPS: {}", 1.0 / delta));
-					});
-
-				// Triangle
-				let vertex_buffer = {
-					glium::VertexBuffer::new(
-						&self.gl_display,
-						&[
-							Vertex {
-								position: [0.0, 0.0],
-								tex_coords: [0.0, 0.0],
-							},
-							Vertex {
-								position: [0.0, 1.0],
-								tex_coords: [0.0, 1.0],
-							},
-							Vertex {
-								position: [1.0, 1.0],
-								tex_coords: [1.0, 1.0],
-							},
-							Vertex {
-								position: [1.0, 0.0],
-								tex_coords: [1.0, 0.0],
-							},
-						],
-					)
-					.unwrap()
-				};
-				let index_buffer = glium::IndexBuffer::new(
-					&self.gl_display,
-					glium::index::PrimitiveType::TriangleStrip,
-					&[1 as u16, 2, 0, 3],
-				)
-				.unwrap();
-
-				// Image
-				let image = image::load(
-					Cursor::new(&include_bytes!("../example.png")),
-					image::ImageFormat::Png,
-				)
-				.unwrap()
-				.to_rgba8();
-				let image_dimensions = image.dimensions();
-				let image = glium::texture::RawImage2d::from_raw_rgba_reversed(
-					&image.into_raw(),
-					image_dimensions,
-				);
-				let texture = glium::texture::SrgbTexture2d::new(&self.gl_display, image).unwrap();
-
-				let uniforms = uniform! {
-					matrix: [
-						[1.0, 0.0, 0.0, 0.0],
-						[0.0, 1.0, 0.0, 0.0],
-						[0.0, 0.0, 1.0, 0.0],
-						[0.0, 0.0, 0.0, 1.0f32],
-					],
-					tex: &texture,
-				};
-
-				let vertex_shader_src = r#"
-					#version 140
-					in vec2 position;
-					in vec2 tex_coords;
-					out vec2 v_tex_coords;
-					uniform mat4 matrix;
-					void main() {
-						v_tex_coords = tex_coords;
-						gl_Position = matrix * vec4(position, 0.0, 1.0);
-					}
-				"#;
-
-				let fragment_shader_src = r#"
-					#version 140
-					in vec2 v_tex_coords;
-					out vec4 color;
-					uniform sampler2D tex;
-					void main() {
-						color = texture(tex, v_tex_coords);
-					}
-				"#;
-
-				let program = glium::Program::from_source(
-					&self.gl_display,
-					vertex_shader_src,
-					fragment_shader_src,
-					None,
-				)
-				.unwrap();
-
-				target
-					.draw(
-						&vertex_buffer,
-						&index_buffer,
-						&program,
-						&uniforms,
-						&Default::default(),
-					)
-					.unwrap();
-
-				// Render that ImGui frame to target
-				self.im_renderer.render(&mut target, ui.render()).unwrap();
-
-				target.finish().unwrap();
+				self.draw();
 			}
 
 			// Set mouse stuff
